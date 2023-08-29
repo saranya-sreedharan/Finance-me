b283b56a0d547067be1ac29764cb9166adf8100b
saranya-sreedharan <37536593+saranya-sreedharan@users.noreply.github.com>
2023-08-29 16:44:08 +0530

    Create test-server.tf

---
+++ test-server.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  key_name      = "salini"
+
+  vpc_security_group_ids = ["sg-0fca6397d8926fb5b"]  
+
+  tags = {
+    Name = "test-server"
+  }
+}
+
+output "server_ip" {
+  value = aws_instance.test_server.public_ip
+}
